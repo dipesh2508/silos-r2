3bf07b7656c0ff29853257f94580978d62b86c8f
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-29 20:14:24 +0530

    cards added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,30 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ApiComponent from '../data/ApiComponent'
+import '../css/cards.css'
 
 const Cards = () => {
-    const data = ApiComponent()
-    console.log(data)
+  const [cards, setCards] = useState([]);
 
-    if (!data) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    async function getData() {
+      const data = await ApiComponent();
+      setCards(data);
     }
-    return (
-        <div className="bg-green-100 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-4">
-            {data.map((item, index) => (
-                <div key={index} className="border p-4 rounded-lg bg-green-200">
-                    <img src={item.image} alt={item.title} className="w-full h-48 object-cover mb-4 rounded-lg" />
-                    <h2 className="text-xl font-bold mb-2 text-green-800">{item.title}</h2>
-                    <p className="text-gray-700 mb-2">{item.category}</p>
-                    <div className="flex items-center mb-2">
-                        <span className="text-yellow-500 mr-1">Rating:</span>
-                        <span className="text-yellow-500">{item.rating.rate}</span>
-                    </div>
-                    <p className="text-gray-700 mb-2">{item.rating.count} ratings</p>
-                    <p className="text-gray-800">{item.description}</p>
+    getData();
+  }, []);
+
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 bg-green-100">
+      {cards.map((card) => (
+
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-8 my-4">
+          <div class="bg-green-300 rounded-lg overflow-hidden shadow-md w-80">
+            <div class="relative">
+              <img src={card.image} alt={card.title} class="w-full h-40 object-cover" />
+              <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity opacity-0 hover:opacity-100">
+                <div class="flex items-center justify-center h-full">
+                  <button class="text-white font-bold text-lg">View Details</button>
                 </div>
-            ))}
+              </div>
+            </div>
+            <div class="p-4 mb-1">
+              <h3 class="text-lg font-semibold">{card.title}</h3>
+              <p class="text-gray-600">{card.category}</p>
+              <div class="flex justify-center mb-1">
+                <span class="text-gray-600">{card.rating.rate}</span>
+              </div>
+            </div>
+          </div>
+
         </div>
-    )
+
+      ))}
+    </div>
+  );
 }
 
 export default Cards
--- src/data/ApiComponent.jsx
+++ src/data/ApiComponent.jsx
@@ -1,24 +1,12 @@
-import { useState, useEffect } from 'react'
+const ApiComponent = async () => {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error fetching data:', error);
+    }
+};
 
-const ApiComponent = () => {
-
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        fetchData();
-    }, [])
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch('https://fakestoreapi.com/products');
-            const jsonData = await response.json();
-            setData(jsonData);
-        } catch (error) {
-            console.log('Error fetching data:', error);
-        }
-    };
-
-    return data
-}
 
 export default ApiComponent
